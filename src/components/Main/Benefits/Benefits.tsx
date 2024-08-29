b6c263d643af31c08e3bfd60106fce0b6dbff4b4
zahar2324 <frontend2024zahar@gmail.com>
2024-08-30 01:09:27 +0300

    Top slider adaptive is finelly ready

--- src/components/Main/Benefits/Benefits.tsx
+++ src/components/Main/Benefits/Benefits.tsx
@@ -9,30 +9,18 @@ interface Benefit {
 }
 
 const benefits: Benefit[] = [
-  {
-    title: 'Сучасне обладнання',
-    text: 'Ми використовуємо передові технології в області лазерної епіляції, що гарантує безпечність і максимальну ефективність процедур.',
-  },
-  {
-    title: 'Професійна команда',
-    text: 'Наші досвідчені спеціалісти забезпечать вам високий рівень обслуговування та індивідуальний підхід.',
-  },
-  {
-    title: 'Зручність та гнучкість',
-    text: 'За допомогою нашого он лайн календаря для запису на візит, плануйте відвідування в зручний для вас час.',
-  },
-  {
-    title: 'Якісні продукти',
-    text: 'Ми використовуємо передові технології в області лазерної епіляції, що гарантує безпечність і максимальну ефективність процедур.',
-  },
-  {
-    title: 'Індивідуальний підхід',
-    text: 'Ми використовуємо передові технології в області лазерної епіляції, що гарантує безпечність і максимальну ефективність процедур.',
-  },
-  {
-    title: 'Доступні ціни',
-    text: 'Ми використовуємо передові технології в області лазерної епіляції, що гарантує безпечність і максимальну ефективність процедур.',
-  },
+  { title: 'Сучасне обладнання', text: 'Ми використовуємо передові технології в області лазерної епіляції, що гарантує безпечність і максимальну ефективність процедур.' },
+  { title: 'Професійна команда', text: 'Наші досвідчені спеціалісти забезпечать вам високий рівень обслуговування та індивідуальний підхід.' },
+  { title: 'Зручність та гнучкість', text: 'За допомогою нашого он лайн календаря для запису на візит, плануйте відвідування в зручний для вас час.' },
+  { title: 'Якісні продукти', text: 'Ми використовуємо передові технології в області лазерної епіляції, що гарантує безпечність і максимальну ефективність процедур.' },
+  { title: 'Індивідуальний підхід', text: 'Ми використовуємо передові технології в області лазерної епіляції, що гарантує безпечність і максимальну ефективність процедур.' },
+  { title: 'Доступні ціни', text: 'Ми використовуємо передові технології в області лазерної епіляції, що гарантує безпечність і максимальну ефективність процедур.' },
+];
+
+// Розділяємо картки на групи по 3
+const groupedBenefits = [
+  benefits.slice(0, 3),
+  benefits.slice(3, 6)
 ];
 
 const Benefits: React.FC = () => {
@@ -48,37 +36,16 @@ const Benefits: React.FC = () => {
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
-  const itemsToShow = 3;
-  const totalSlides = isMobile
-    ? Math.ceil(benefits.length / itemsToShow)
-    : benefits.length;
+  const totalSlides = groupedBenefits.length;
 
-  const nextSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 3) % totalSlides);
-  };
-
-  const prevSlide = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? totalSlides - 3 : prevIndex - 3
-    );
-  };
-
-  const handlers = useSwipeable({
-    onSwipedLeft: nextSlide,
-    onSwipedRight: prevSlide,
+  const commonHandlers = useSwipeable({
+    onSwipedLeft: () => setCurrentIndex(prevIndex => Math.min(prevIndex + 1, totalSlides - 1)),
+    onSwipedRight: () => setCurrentIndex(prevIndex => Math.max(prevIndex - 1, 0)),
     trackMouse: true,
   });
 
-  const groupedBenefits = isMobile
-    ? benefits.reduce((resultArray: Benefit[][], item, index) => {
-        const chunkIndex = Math.floor(index / itemsToShow);
-        if (!resultArray[chunkIndex]) {
-          resultArray[chunkIndex] = []; // start a new chunk
-        }
-        resultArray[chunkIndex].push(item);
-        return resultArray;
-      }, [])
-    : [benefits];
+  const handleNext = () => setCurrentIndex(prevIndex => Math.min(prevIndex + 1, totalSlides - 1));
+  const handlePrev = () => setCurrentIndex(prevIndex => Math.max(prevIndex - 1, 0));
 
   return (
     <div className="page__benefits benefits">
@@ -87,69 +54,43 @@ const Benefits: React.FC = () => {
       </div>
       <div className="benefits__items">
         <div className="benefits__slider">
-          {isMobile ? (
-            <>
-              <button className="slider__control slider__control--prev" onClick={prevSlide}>
-                {/* SVG Icon for Previous */}
-              </button>
-              <div
-                className="slider__slides"
-                style={{
-                  transform: `translateX(-${currentIndex * (230 / itemsToShow)}%)`,
-                }}
-                {...handlers}
-              >
-                {groupedBenefits.map((group, index) => (
-                  <div key={index} className="slider__group">
-                    {group.map((benefit, i) => (
-                      <div key={i} className="benefits__item benefits-item">
-                        <div className="benefits-item__title">
-                          <span>{index * itemsToShow + i + 1}</span> {benefit.title}
-                        </div>
-                        <div className="benefits-item__text">
-                          {benefit.text}
-                        </div>
-                      </div>
-                    ))}
-                  </div>
-                ))}
-              </div>
-              <button className="slider__control slider__control--next" onClick={nextSlide}>
-                {/* SVG Icon for Next */}
-              </button>
-            </>
-          ) : (
-            <>
-              <button className="slider__control slider__control--prev" onClick={prevSlide}>
-                <svg width="52" height="17" viewBox="0 0 52 17" fill="none" xmlns="http://www.w3.org/2000/svg" className="slider-icon">
-                  <path d="M1 8.49992L0.646448 8.14636L0.292893 8.49991L0.646445 8.85347L1 8.49992ZM51 8.99992C51.2761 8.99992 51.5 8.77606 51.5 8.49992C51.5 8.22378 51.2761 7.99992 51 7.99992V8.99992ZM8.64645 0.146445L0.646448 8.14636L1.35355 8.85347L9.35355 0.853555L8.64645 0.146445ZM0.646445 8.85347L8.64644 16.8536L9.35356 16.1464L1.35356 8.14637L0.646445 8.85347ZM1 8.99992H51V7.99992H1V8.99992Z" className="slider-icon-path"/>
-                </svg>
-              </button>
-              <div
-                className="slider__slides"
-                style={{
-                  transform: `translateX(-${currentIndex * (100 / itemsToShow)}%)`,
-                }}
-                {...handlers}
-              >
-                {benefits.map((benefit, index) => (
-                  <div key={index} className="benefits__item benefits-item">
+          <button className="slider__control slider__control--prev" onClick={handlePrev}>
+          <svg width="52" height="17" viewBox="0 0 52 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M1 8.49992L0.646448 8.14636L0.292893 8.49991L0.646445 8.85347L1 8.49992ZM51 8.99992C51.2761 8.99992 51.5 8.77606 51.5 8.49992C51.5 8.22378 51.2761 7.99992 51 7.99992V8.99992ZM8.64645 0.146445L0.646448 8.14636L1.35355 8.85347L9.35355 0.853555L8.64645 0.146445ZM0.646445 8.85347L8.64644 16.8536L9.35356 16.1464L1.35356 8.14637L0.646445 8.85347ZM1 8.99992H51V7.99992H1V8.99992Z" fill="#8E8E8E"/>
+</svg>
+
+
+
+          </button>
+          <div
+            className="slider__slides"
+            style={{
+              transform: `translateX(-${currentIndex * 100}%)`,
+              transition: 'transform 0.3s ease',
+            }}
+            {...commonHandlers}
+          >
+            {groupedBenefits.map((group, index) => (
+              <div key={index} className={`slider__group slider__group--${index + 1}`}>
+                {group.map((benefit, i) => (
+                  <div key={i} className="benefits__item benefits-item">
                     <div className="benefits-item__title">
-                      <span>{index + 1}</span> {benefit.title}
+                      <span>{index * 3 + i + 1}</span> <p>{benefit.title}</p> 
                     </div>
                     <div className="benefits-item__text">
-                      {benefit.text}
+                      <p>{benefit.text}</p> 
                     </div>
                   </div>
                 ))}
               </div>
-              <button className="slider__control slider__control--next" onClick={nextSlide}>
-                <svg width="51" height="17" viewBox="0 0 51 17" fill="none" xmlns="http://www.w3.org/2000/svg" className="slider-icon">
-                  <path d="M51 8.50008L51.3536 8.85364L51.7071 8.50009L51.3536 8.14653L51 8.50008ZM1 8.00008C0.723858 8.00008 0.5 8.22394 0.5 8.50008C0.5 8.77622 0.723858 9.00008 1 9.00008V8.00008ZM43.3536 16.8536L51.3536 8.85364L50.6464 8.14653L42.6464 16.1464L43.3536 16.8536ZM51.3536 8.14653L43.3536 0.146448L42.6464 0.853552L50.6464 8.85363L51.3536 8.14653ZM51 8.00008L1 8.00008V9.00008L51 9.00008V8.00008Z" className="slider-icon-path"/>
-                </svg>
-              </button>
-            </>
-          )}
+            ))}
+          </div>
+          <button className="slider__control slider__control--next" onClick={handleNext}>
+          <svg width="51" height="17" viewBox="0 0 51 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M51 8.50008L51.3536 8.85364L51.7071 8.50009L51.3536 8.14653L51 8.50008ZM1 8.00008C0.723858 8.00008 0.5 8.22394 0.5 8.50008C0.5 8.77622 0.723858 9.00008 1 9.00008V8.00008ZM43.3536 16.8536L51.3536 8.85364L50.6464 8.14653L42.6464 16.1464L43.3536 16.8536ZM51.3536 8.14653L43.3536 0.146448L42.6464 0.853552L50.6464 8.85363L51.3536 8.14653ZM51 8.00008L1 8.00008V9.00008L51 9.00008V8.00008Z" fill="black"/>
+</svg>
+
+          </button>
         </div>
       </div>
     </div>
